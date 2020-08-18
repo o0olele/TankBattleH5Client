21a92482eaa6b46b397467dade29588066160b07
lele <lr18516312@163.com>
2020-08-18 15:36:03 +0800

    [game] update

--- js/custom-game.js
+++ js/custom-game.js
@@ -10,3 +10,32 @@ function GetRequest() {
     }
     return theRequest;
 }
+
+function UserObject() {
+    this.id = 0;
+    this.x = 0;
+    this.y = 0;
+    this.mesh = null;
+}
+
+UserObject.prototype.Init = function (id, x, y) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+
+    var geo = new THREE.BoxGeometry(1, 1, 1);
+    var mtr = new THREE.MeshBasicMaterial({ color: 0x66ccff });
+    this.mesh = new THREE.Mesh(geo, mtr);
+}
+
+UserObject.prototype.UpdatePos = function (x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.UpdateMesh();
+}
+
+UserObject.prototype.UpdateMesh = function () {
+    this.mesh.position.x = this.x;
+    this.mesh.position.y = this.y;
+}
